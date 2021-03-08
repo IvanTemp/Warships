ec05d209ec846fa19a93f4ddb856a6f1088c6e45
Alexey Romanov <aisek72@gmail.com>
2021-03-08 21:39:16 +0300

    Вжух и у нас два флота

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -11,32 +11,33 @@ int main()
 
 	//Получение урона
 	//Создадим вектор флот
-	std::vector<ship> fleet; //TODO - СДЕЛАТЬ TUPLE
+	std::vector<ship> fleet_1;
+	std::vector<ship> fleet_2;
 	//Запихнем в вектор флот наши кораблики
-	fleet.push_back({ "Enterprise", "Aircraft Carrier", 0 });
-	fleet.push_back({ "Prinz Eugene1", "Heavy Cruiser", 1 });
-	fleet.push_back({ "Prinz Eugene2", "Heavy Cruiser", 2 });
-	fleet.push_back({ "FLX1", "Tsundere", 3 });
-	fleet.push_back({ "FLX2", "Tsundere", 4 });
-	fleet.push_back({ "FLX3", "Tsundere", 5 });
-	fleet.push_back({ "Flaffey1", "Small", 6 });
-	fleet.push_back({ "Flaffey2", "Small", 7 });
-	fleet.push_back({ "Flaffey3", "Small", 8 });
-	fleet.push_back({ "Flaffey4", "Small", 9 });
-	Generate_ships_in_random_places(fleet[0], 1);
+	fleet_1.push_back({ "Enterprise", "Aircraft Carrier", 0 });
+	fleet_1.push_back({ "Prinz Eugene1", "Heavy Cruiser", 1 });
+	fleet_1.push_back({ "Prinz Eugene2", "Heavy Cruiser", 2 });
+	fleet_1.push_back({ "FLX1", "Tsundere", 3 });
+	fleet_1.push_back({ "FLX2", "Tsundere", 4 });
+	fleet_1.push_back({ "FLX3", "Tsundere", 5 });
+	fleet_1.push_back({ "Flaffey1", "Small", 6 });
+	fleet_1.push_back({ "Flaffey2", "Small", 7 });
+	fleet_1.push_back({ "Flaffey3", "Small", 8 });
+	fleet_1.push_back({ "Flaffey4", "Small", 9 });
+	Generate_ships_in_random_places(fleet_1[0], 1);
 	std::cout << std::endl << std::endl << std::endl;
 	std::cout << "ID: \n\n";
-	Output_Field_ID(1);
+	Output_Field_ID(0);
 	std::cout << std::endl << std::endl << std::endl;
 	std::cout << "War: \n\n";
-	Output_Field_War(1);
+	Output_Field_War(0);
 	std::cout << std::endl << std::endl << std::endl;
 	std::cout << "Final: \n\n";
-	Output_Field_Final(1);
+	Output_Field_Final(0);
 	std::cout << std::endl << std::endl << std::endl;
 	//Выведем флот
 	std::cout << "======Fleet=======" << std::endl;
-	for (const auto& x : fleet)
+	for (const auto& x : fleet_1)
 	{
 		x.Print();
 	}
