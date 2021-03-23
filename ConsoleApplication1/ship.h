228cedca4099ab68759d20288ff1a6d8e9927cd1
Alexey Romanov <aisek72@gmail.com>
2021-03-23 10:49:00 +0300

    Забыл упомянуть, ввёл definde Default_Damage для облегчения теста. В дальнейшем у каждого типа корабля будет свой урон

--- ConsoleApplication1/ship.h
+++ ConsoleApplication1/ship.h
@@ -9,7 +9,7 @@
 #define Heavy_Cruiser_Durability 7
 #define Tsundere_Durability 5
 #define Small_Durability 4
-#define Default_Damage 100
+#define Default_Damage 2
 #define DEBUG_MODE 1
 
 class ship
