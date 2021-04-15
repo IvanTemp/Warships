9ebf4b41aa36340ce4ea84c97e0f986918e830e0
Alexey Romanov <aisek72@gmail.com>
2021-04-15 17:56:23 +0300

    чё

--- ConsoleApplication1/fleet.h
+++ ConsoleApplication1/fleet.h
@@ -27,7 +27,8 @@ public:
 	void damage_by_index_player(Ship &sheep);
 	void heavy_cruiser_attack_bot(const int dmg, int difficulty);
 	void aircraft_attack_player(const bool angle, const int dmg);
-	void aircraft_attack_bot(const bool angle, const int dmg, int difficulty);
+	void aircraft_attack_bot(const int dmg, int difficulty);
+	void find_founded_ships(std::vector <std::pair <unsigned int, unsigned int>>& memory);
 	void heavy_cruiser_attack_player(const int dmg);
 	void nuclear_bomb();
 	Fleet& operator +=(const Ship& ship); //add ship
