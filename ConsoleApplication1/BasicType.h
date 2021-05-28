baf9910c78346001b15fc14ed9ffae21542907ec
Alexey Romanov <aisek72@gmail.com>
2021-05-28 13:52:16 +0300

    Убрал неразбериху с именем корабля и именем класса

--- ConsoleApplication1/BasicType.h
+++ ConsoleApplication1/BasicType.h
@@ -5,9 +5,9 @@
 class BasicType {
 public:
 	virtual ~BasicType() {};
-	std::string get_name() { return name_; };
-	void set_name(std::string nm) { this->name_ = nm; };
-	void print() { std::cout << name_; };
+	std::string get_name() { return typename_; };
+	void set_name(std::string nm) { this->typename_ = nm; };
+	void print() { std::cout << typename_; };
 	virtual int get_size()const = 0;
 	virtual int get_damage_value()const = 0;
 	virtual int get_default_durability()const = 0;
@@ -15,5 +15,5 @@ public:
 	virtual std::string get_description()const = 0;
 	
 private:
-	std::string name_;
+	std::string typename_;
 };
